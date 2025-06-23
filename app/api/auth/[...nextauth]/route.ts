11f78a356a73c3ffb7d6854430c901c495828e2b
luksuz <lukamindjek@gmail.com>
2025-06-23 13:53:01 +0200

    fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,4 @@
 // app/api/auth/[...nextauth]/route.ts
-import { authOptions } from '@/utils/authOptions';
-import NextAuth from 'next-auth';
+import { handlers } from '@/utils/authOptions';
 
-
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+export const { GET, POST } = handlers;
